5c7c7f30ccaa07c0e78dd9469cac56243dad4c5b
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2020-10-05 21:48:58 +0000

    refactored hover screenshots for DesktopPlayerControls to be more robust

--- tests/e2e/playback/controls/player/desktop-player-tooltips-and-hover.e2e.js
+++ tests/e2e/playback/controls/player/desktop-player-tooltips-and-hover.e2e.js
@@ -35,13 +35,15 @@ const active_record_button = Selector(
 const whole_component = Selector(".div__play-desktop-player-controls");
 const title_text = Selector(".span__playback-desktop-player-controls-text");
 
+const msec_to_wait_for_tooltip_after_hover_screenshot = 1200;
+
 // the fixture declares what we are testing
 fixture`playback/controls/player/desktop-player/initial-tooltips`.page(
   // declare the fixture
   `http://localhost:8080/playback/controls/player/desktop-player/initial-tooltips`
 ); // specify the start page
 
-test("DesktopPlayerControls UI tooltips at INITIAL State settings schedule calibrate live-view recording", async (t) => {
+test("Given Vuex is in the default player state and a valid Barcode has been committed, When hovering on the elements, Then the hover behavior occurs followed by the correct tooltip", async (t) => {
   const this_base_screenshot_path = path.join(
     base_screenshot_path,
     "initial-tooltips"
@@ -64,22 +66,39 @@ test("DesktopPlayerControls UI tooltips at INITIAL State settings schedule calib
   await t.wait(2000);
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "calibrate-tooltip");
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "still-initializing--hover-on-calibrate"
+  );
   await t.hover(calibrate_button);
-  // wait for tooltips to visually display state to complete
-  await t.wait(2000);
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "live-view-tooltip");
+  // wait for tooltips to visually display state to complete
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "calibrate-tooltip");
+  await testcafe_page_visual_regression(t, screenshot_path);
+
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "still-initializing--hover-on-live-view"
+  );
   await t.hover(live_view_button);
-  // wait for tooltips to visually display state to complete
-  await t.wait(2000);
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "record-tooltip");
-  await t.hover(record_button);
   // wait for tooltips to visually display state to complete
-  await t.wait(2000);
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "live-view-tooltip");
+  await testcafe_page_visual_regression(t, screenshot_path);
+
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "still-initializing--hover-on-record"
+  );
+  await t.hover(record_button);
+  await testcafe_page_visual_regression(t, screenshot_path);
+  // wait for tooltips to visually display state to complete
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "record-tooltip");
   await testcafe_page_visual_regression(t, screenshot_path);
 });
 
@@ -89,7 +108,7 @@ fixture`playback/controls/player/desktop-player/needs-calibration-tooltips`.page
   `http://localhost:8080/playback/controls/player/desktop-player/needs-calibration-tooltips`
 ); // specify the start page
 
-test("DesktopPlayerControls UI tooltips at NEEDS CALIBRATION settings schedule calibrate live-view recording", async (t) => {
+test("Given that Vuex is in the CALIBRATION_NEEDED state and a valid barcode is committed to the store, When hovering on the elements, Then the hover behavior occurs followed by the correct tooltip", async (t) => {
   const this_base_screenshot_path = path.join(
     base_screenshot_path,
     "needs-calibration-tooltips"
@@ -112,22 +131,38 @@ test("DesktopPlayerControls UI tooltips at NEEDS CALIBRATION settings schedule c
   await t.wait(2000);
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "calibrate-tooltip");
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "needs-calibration--hover-on-calibrate"
+  );
   await t.hover(calibrate_button);
+  await testcafe_page_visual_regression(t, screenshot_path);
   // wait for tooltips to visually display state to complete
-  await t.wait(2000);
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "calibrate-tooltip");
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "live-view-tooltip");
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "needs-calibration--hover-on-live-view"
+  );
   await t.hover(live_view_button);
-  // wait for tooltips to visually display state to complete
-  await t.wait(2000);
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "record-tooltip");
-  await t.hover(record_button);
   // wait for tooltips to visually display state to complete
-  await t.wait(2000);
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "live-view-tooltip");
+  await testcafe_page_visual_regression(t, screenshot_path);
+
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "needs-calibration--hover-on-record"
+  );
+  await t.hover(record_button);
+  await testcafe_page_visual_regression(t, screenshot_path);
+  // wait for tooltips to visually display state to complete
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "record-tooltip");
   await testcafe_page_visual_regression(t, screenshot_path);
 });
 
@@ -137,7 +172,7 @@ fixture`playback/controls/player/desktop-player/calibrated-tooltips`.page(
   `http://localhost:8080/playback/controls/player/desktop-player/calibrated-tooltips`
 ); // specify the start page
 
-test("DesktopPlayerControls UI tooltips at CALIBIRATED STATE settings schedule calibrate live-view recording", async (t) => {
+test("Given that Vuex is in the CALIBRATED state and a valid barcode is committed to the store, When hovering on the elements, Then the hover behavior occurs followed by the correct tooltip", async (t) => {
   const this_base_screenshot_path = path.join(
     base_screenshot_path,
     "calibrated-tooltips"
@@ -160,22 +195,37 @@ test("DesktopPlayerControls UI tooltips at CALIBIRATED STATE settings schedule c
   await t.wait(2000);
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "calibrate-tooltip");
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "calibrated--hover-on-calibrate"
+  );
   await t.hover(calibrate_button);
+  await testcafe_page_visual_regression(t, screenshot_path);
   // wait for tooltips to visually display state to complete
-  await t.wait(2000);
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "calibrate-tooltip");
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "live-view-tooltip");
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "calibrated--hover-on-live-view"
+  );
   await t.hover(live_view_button);
+  await testcafe_page_visual_regression(t, screenshot_path);
   // wait for tooltips to visually display state to complete
-  await t.wait(2000);
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "live-view-tooltip");
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "record-tooltip");
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "calibrated--hover-on-record"
+  );
   await t.hover(record_button);
+  await testcafe_page_visual_regression(t, screenshot_path);
   // wait for tooltips to visually display state to complete
-  await t.wait(2000);
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "record-tooltip");
   await testcafe_page_visual_regression(t, screenshot_path);
 });
 
@@ -185,7 +235,7 @@ fixture`playback/controls/player/desktop-player/live-view-tooltips`.page(
   `http://localhost:8080/playback/controls/player/desktop-player/live-view-tooltips`
 ); // specify the start page
 
-test("DesktopPlayerControls UI tooltips at LIVE VIEW settings schedule calibrate live-view recording", async (t) => {
+test("Given that Vuex is in the LIVE_VIEW_ACTIVE state and a valid barcode is committed to the store, When hovering on the elements, Then the hover behavior occurs followed by the correct tooltip", async (t) => {
   const this_base_screenshot_path = path.join(
     base_screenshot_path,
     "live-view-tooltips"
@@ -208,22 +258,39 @@ test("DesktopPlayerControls UI tooltips at LIVE VIEW settings schedule calibrate
   await t.wait(2000);
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "calibrate-tooltip");
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "live-view-active--hover-on-calibrate"
+  );
   await t.hover(calibrate_button);
+  await testcafe_page_visual_regression(t, screenshot_path);
   // wait for tooltips to visually display state to complete
-  await t.wait(2000);
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "calibrate-tooltip");
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "live-view-tooltip");
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "live-view-active--hover-on-live-view"
+  );
   await t.hover(live_view_button);
+  await testcafe_page_visual_regression(t, screenshot_path);
   // wait for tooltips to visually display state to complete
-  await t.wait(2000);
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "live-view-tooltip");
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "record-tooltip");
+  // await t.wait(1000); // Eli (7/1/20): odd issue where there was a tiny 1 pixel difference in CodeBuild but not locally...hopefully this wait solves it...
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "live-view-active--hover-on-record"
+  );
+
   await t.hover(record_button);
+  await testcafe_page_visual_regression(t, screenshot_path);
   // wait for tooltips to visually display state to complete
-  await t.wait(2000);
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "record-tooltip");
   await testcafe_page_visual_regression(t, screenshot_path);
 });
 
@@ -233,7 +300,7 @@ fixture`playback/controls/player/desktop-player/recording-tooltips`.page(
   `http://localhost:8080/playback/controls/player/desktop-player/recording-tooltips`
 ); // specify the start page
 
-test("DesktopPlayerControls UI tooltips at RECORD STATE settings schedule calibrate live-view recording", async (t) => {
+test("Given that Vuex is in the RECORDING state and a valid barcode is committed to the store, When hovering on the elements, Then the hover behavior occurs followed by the correct tooltip", async (t) => {
   const this_base_screenshot_path = path.join(
     base_screenshot_path,
     "recording-tooltips"
@@ -256,21 +323,36 @@ test("DesktopPlayerControls UI tooltips at RECORD STATE settings schedule calibr
   await t.wait(2000);
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "calibrate-tooltip");
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "recording--hover-on-calibrate"
+  );
   await t.hover(calibrate_button);
+  await testcafe_page_visual_regression(t, screenshot_path);
   // wait for tooltips to visually display state to complete
-  await t.wait(2000);
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "calibrate-tooltip");
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "live-view-tooltip");
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "recording--hover-on-live-view"
+  );
   await t.hover(live_view_button);
+  await testcafe_page_visual_regression(t, screenshot_path);
   // wait for tooltips to visually display state to complete
-  await t.wait(2000);
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "live-view-tooltip");
   await testcafe_page_visual_regression(t, screenshot_path);
 
-  screenshot_path = path.join(this_base_screenshot_path, "record-tooltip");
+  screenshot_path = path.join(
+    this_base_screenshot_path,
+    "recording--hover-on-record"
+  );
   await t.hover(active_record_button);
+  await testcafe_page_visual_regression(t, screenshot_path);
   // wait for tooltips to visually display state to complete
-  await t.wait(2000);
+  await t.wait(msec_to_wait_for_tooltip_after_hover_screenshot);
+  screenshot_path = path.join(this_base_screenshot_path, "record-tooltip");
   await testcafe_page_visual_regression(t, screenshot_path);
 });
