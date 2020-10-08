ba093227e19b3039e30b73513f3287927b057b8b
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2020-10-08 16:10:26 +0000

    fixed to match new @curibio npm package hyphens

--- tests/e2e/playback/controls/plate-well.e2e.js
+++ tests/e2e/playback/controls/plate-well.e2e.js
@@ -2,7 +2,7 @@ import { Selector } from "testcafe";
 
 const path = require("path");
 
-import { testcafe_page_visual_regression } from "@curibio/frontend_test_utils";
+import { testcafe_page_visual_regression } from "@curi-bio/frontend-test-utils";
 const base_screenshot_path = path.join("playback", "controls", "plate-well");
 
 // the fixture declares what we are testing
