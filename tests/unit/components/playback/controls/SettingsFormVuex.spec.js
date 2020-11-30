168772b838325250fc0dad5d9ab4a3ad13d6c181
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2020-11-30 17:20:54 +0000

    refactored SettingsFormVuex

--- tests/unit/components/playback/controls/SettingsFormVuex.spec.js
+++ tests/unit/components/playback/controls/SettingsFormVuex.spec.js
@@ -6,6 +6,7 @@ import Vuex from "vuex";
 import { createLocalVue } from "@vue/test-utils";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { array_of_customer_ids } from "./SettingsFormCustomerData.js";
 
 let wrapper = null;
 
@@ -28,46 +29,7 @@ describe("SettingsForm.vue", () => {
   });
 
   afterEach(() => wrapper.destroy());
-  const array_of_userid_1 = [
-    {
-      user_id: 0,
-      uuid: "2VSckkBYr2An3dqHEyfRRE",
-      nickname: "User account -1",
-    },
-    {
-      user_id: 1,
-      uuid: "5FY8KwTsQaUJ2KzHJGetfE",
-      nickname: "User account -2",
-    },
-  ];
-  const array_of_userid_2 = [
-    {
-      user_id: 0,
-      uuid: "2VSckkkkk2An3dqHEyfRRE",
-      nickname: "Lab User -1",
-    },
-    {
-      user_id: 1,
-      uuid: "5FY8ghtsQaUJ2KzHJGetfE",
-      nickname: "Intern -1",
-    },
-  ];
-  const array_of_customer_ids = [
-    {
-      cust_id: 0,
-      uuid: "4vqyd62oARXqj9nRUNhtLQ",
-      api_key: "941532a0-6be1-443a-a9d5-d57bdf180a52",
-      nickname: "Customer account -1",
-      user_ids: array_of_userid_1,
-    },
-    {
-      cust_id: 1,
-      uuid: "6cBaidlJ84Ggc5JA7IYCgv",
-      api_key: "941532a0-6be1-443a-cdee-d57bdf180a52",
-      nickname: "Customer account -2",
-      user_ids: array_of_userid_2,
-    },
-  ];
+
   const array_of_customer_ids_missing_user_ids = [
     {
       cust_id: 0,
@@ -98,27 +60,62 @@ describe("SettingsForm.vue", () => {
     expect(target_span.text()).toStrictEqual("Settings");
   });
 
-  test("Given that the SettingsForm is loaded with Vuex store, When user action which generates an event 'cancel-id', Then the Vuex Store is not modified", async () => {
-    store.commit("settings/set_customer_account_ids", array_of_customer_ids);
-    store.commit("settings/set_customer_index", 1);
-
-    wrapper = mount(ComponentToTest, {
-      store,
-      localVue,
+  describe("Given Vuex has valid customer and user accounts but no customer index or user index selected", () => {
+    beforeEach(() => {
+      // commit a deep copy of the template object to the Vuex store using JSON stringify/parse, as it may be modified during tests. https://www.javascripttutorial.net/object/3-ways-to-copy-objects-in-javascript/
+      store.commit(
+        "settings/set_customer_account_ids",
+        JSON.parse(JSON.stringify(array_of_customer_ids))
+      );
     });
+    test("Given that a customer account is selected in Vuex, When the method handling the 'cancel-id' customer event is invoked, Then the Vuex Store account data or selected customer account index is not modified", async () => {
+      store.commit("settings/set_customer_index", 1);
 
-    wrapper.vm.onCancelCustomerId();
-    expect(store.getters["settings/customer_account_ids"]).toStrictEqual(
-      array_of_customer_ids
-    );
-    expect(store.getters["settings/customer_index"]).toStrictEqual(1); // this is the real data due to savechanges function Vuex stored data of customer_index
+      wrapper = mount(ComponentToTest, {
+        store,
+        localVue,
+      });
+
+      wrapper.vm.onCancelCustomerId();
+      expect(store.getters["settings/customer_account_ids"]).toStrictEqual(
+        array_of_customer_ids
+      );
+      expect(store.getters["settings/customer_index"]).toStrictEqual(1); // this is the real data due to savechanges function Vuex stored data of customer_index
+    });
+    test("Given a customer and user account selected in Vuex and the textbox for Customer Account is changed to an account different than the one in Vuex and a user account is selected in thet textbox, When the Save Changes button is clicked, Then the selected indices in Vuex for Customer and User accounts are updated to reflect the chosen options in the textboxes", async () => {
+      store.commit("settings/set_customer_index", 0);
+      store.commit("settings/set_user_index", 0);
+
+      wrapper = mount(ComponentToTest, {
+        store,
+        localVue,
+      });
+
+      await wrapper
+        .find("#input-dropdown-widget-cust-")
+        .setValue("Customer account -2");
+      await wrapper.vm.$nextTick(); // wait for update
+      await wrapper
+        .find("#input-dropdown-widget-user-")
+        .setValue("Lab User  -1");
+      await wrapper.vm.$nextTick(); // wait for update
+
+      const save_changes = wrapper.find(
+        ".span__settings-tool-tip-save-btn-txt-enable"
+      );
+      await save_changes.trigger("click");
+
+      await wrapper.vm.$nextTick(); // wait for update
+
+      expect(store.getters["settings/customer_index"]).toStrictEqual(1); // this is the real data due to savechanges function Vuex stored data of customer_index
+      expect(store.getters["settings/user_index"]).toStrictEqual(0); // this is the real data due to savechanges function Vuex stored data of user_index
+    });
   });
   test("Given that no data are in the Vuex store, When the component is mounted, Then verify that Input of Customer ID and User ID are <empty>", () => {
     wrapper = mount(ComponentToTest, {
       store,
       localVue,
     });
-    wrapper.vm.onCancelAddCustomerId(); // Complete Code Coverage
     expect(
       wrapper.find("#input-dropdown-widget-cust-").element.value
     ).toStrictEqual("");
@@ -159,36 +156,6 @@ describe("SettingsForm.vue", () => {
     expect(
       wrapper.find("#input-dropdown-widget-user-").element.value
     ).toStrictEqual("");
-  });
-  test("Given that the SettingsForm is loaded with Vuex with 'Customer account -1' 'User account -1', When user decides to modify Customer ID to 'Customer account -2' 'Lab User -1', Then validate 'Save Changes' updates validate Vuex store with is updated index values", async () => {
-    store.commit("settings/set_customer_account_ids", array_of_customer_ids);
-    store.commit("settings/set_customer_index", 0);
-    store.commit("settings/set_user_index", 0);
-
-    wrapper = mount(ComponentToTest, {
-      store,
-      localVue,
-    });
-    await wrapper.vm.$nextTick(); // wait for update
-
-    await wrapper
-      .find("#input-dropdown-widget-cust-")
-      .setValue("Customer account -2");
-    await wrapper.vm.$nextTick(); // wait for update
-    await wrapper.find("#input-dropdown-widget-user-").setValue("Lab User -1");
-    await wrapper.vm.$nextTick(); // wait for update
-
-    await wrapper.vm.$nextTick(); // wait for update
-
-    const save_changes = wrapper.find(
-      ".span__settings-tool-tip-save-btn-txt-enable"
-    );
-    await save_changes.trigger("click");
-
-    await wrapper.vm.$nextTick(); // wait for update
-
-    expect(store.getters["settings/customer_index"]).toStrictEqual(1); // this is the real data due to savechanges function Vuex stored data of customer_index
-    expect(store.getters["settings/user_index"]).toStrictEqual(0); // this is the real data due to savechanges function Vuex stored data of user_index
   });
   // Eli (11/25/20): commenting out this test until we are ready to implement the feature
   // test("Given that the SettingsForm is loaded with Vuex, When the 'Key Icon' decoder is invoked, Then validate if the decoder string converts UNICODE value to key icon", async () => {
