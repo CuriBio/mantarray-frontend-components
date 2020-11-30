c2a711d2d78f0d3c9e4ae363523c11d4391f4651
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2020-11-30 17:09:55 +0000

    refactored SettingsFormDropDown

--- tests/unit/components/playback/controls/SettingsFormDropDown.spec.js
+++ tests/unit/components/playback/controls/SettingsFormDropDown.spec.js
@@ -31,32 +31,92 @@ describe("SettingsForm.vue", () => {
   });
 
   afterEach(() => wrapper.destroy());
-
-  test("When the SettingsForm has two Customer ID in the Vuex, Then validate that dropDown of customer contains the nicknames 'Customer account -1' and 'Customer account -2'", () => {
-    store.commit("settings/set_customer_account_ids", array_of_customer_ids);
-    wrapper = mount(ComponentToTest, {
-      store,
-      localVue,
+  describe("Given Vuex has valid customer and user accounts but no customer index or user index selected", () => {
+    beforeEach(() => {
+      // commit a deep copy of the template object to the Vuex store using JSON stringify/parse, as it may be modified during tests. https://www.javascripttutorial.net/object/3-ways-to-copy-objects-in-javascript/
+      store.commit(
+        "settings/set_customer_account_ids",
+        JSON.parse(JSON.stringify(array_of_customer_ids))
+      );
     });
-    expect(wrapper.find("#cust-0").text()).toStrictEqual("Customer account -1");
-    expect(wrapper.find("#cust-1").text()).toStrictEqual("Customer account -2");
-  });
-  test("When the SettingsForm has two Customer ID and two User ID per Customer ID in the Vuex, Then validate that dropDown of user contains the nicknames 'Lab User  -1' and 'Intern -1'", async () => {
-    store.commit("settings/set_customer_account_ids", array_of_customer_ids);
-    store.commit("settings/set_customer_index", 0);
-    store.commit("settings/set_user_index", 0);
-    wrapper = mount(ComponentToTest, {
-      store,
-      localVue,
+    test("When the SettingsForm is mounted, Then the dropDown of customer contains the nicknames from Vuex", () => {
+      wrapper = mount(ComponentToTest, {
+        store,
+        localVue,
+      });
+      expect(wrapper.find("#cust-0").text()).toStrictEqual(
+        "Customer account -1"
+      );
+      expect(wrapper.find("#cust-1").text()).toStrictEqual(
+        "Customer account -2"
+      );
+    });
+    test("Given a customer and user are selected in Vuex, When a different customer account is selected in the component, Then the dropdown choices for the User update to the users in the newly selected customer account", async () => {
+      store.commit("settings/set_customer_index", 0);
+      store.commit("settings/set_user_index", 0);
+      wrapper = mount(ComponentToTest, {
+        store,
+        localVue,
+      });
+
+      await wrapper
+        .find("#input-dropdown-widget-cust-")
+        .setValue("Customer account -2");
+      expect(wrapper.find("#user-0").text()).toStrictEqual("Lab User  -1");
+      expect(wrapper.find("#user-1").text()).toStrictEqual("Intern -1");
     });
 
-    await wrapper
-      .find("#input-dropdown-widget-cust-")
-      .setValue("Customer account -2");
-    expect(wrapper.find("#user-0").text()).toStrictEqual("Lab User  -1");
-    expect(wrapper.find("#user-1").text()).toStrictEqual("Intern -1");
+    test("When the Component is mounted, Then the dropdown menu for the Customer Account matches the values in Vuex", async () => {
+      wrapper = mount(ComponentToTest, {
+        store,
+        localVue,
+      });
+      expect(wrapper.find("#cust-0").text()).toStrictEqual(
+        "Customer account -1"
+      );
+      expect(wrapper.find("#cust-1").text()).toStrictEqual(
+        "Customer account -2"
+      );
+      expect(wrapper.find("#cust-2").exists()).toBe(false); // confirm only the 2 accounts in Vuex are present
+    });
+    test("When a customer account is selected in the textbox, Then the user account dropdown populates with the users for that customer account", async () => {
+      wrapper = mount(ComponentToTest, {
+        store,
+        localVue,
+      });
+
+      await wrapper
+        .find("#input-dropdown-widget-cust-")
+        .setValue("Customer account -2");
+      expect(wrapper.find("#user-0").text()).toStrictEqual("Lab User  -1");
+      expect(wrapper.find("#user-1").text()).toStrictEqual("Intern -1");
+    });
+    test("Given a customer account is selected in the textbox, When a new customer account is selected in the textbox, Then the user account dropdown populates with the users for that newly selected customer account", async () => {
+      wrapper = mount(ComponentToTest, {
+        store,
+        localVue,
+      });
+
+      await wrapper
+        .find("#input-dropdown-widget-cust-")
+        .setValue("Customer account -2");
+      // confirm pre-condition
+      expect(wrapper.find("#user-0").text()).not.toStrictEqual(
+        "User account -1"
+      );
+      expect(wrapper.find("#user-1").text()).not.toStrictEqual(
+        "User account -2"
+      );
+
+      await wrapper
+        .find("#input-dropdown-widget-cust-")
+        .setValue("Customer account -1");
+
+      expect(wrapper.find("#user-0").text()).toStrictEqual("User account -1");
+      expect(wrapper.find("#user-1").text()).toStrictEqual("User account -2");
+    });
   });
-  test("When the SettingsForm has two Customer ID and one User ID for first Customer ID and no User ID for second Customer ID in the Vuex, Then validate that the User ID for second Customer ID does not exist", () => {
+  test("Given a customer account is selected in Vuex with no associated users, When the component is mounted, Then there are no dropdown options for the user list", () => {
     store.commit(
       "settings/set_customer_account_ids",
       array_of_customer_ids_one
@@ -69,29 +129,5 @@ describe("SettingsForm.vue", () => {
 
     expect(wrapper.find("#user-0").exists()).toBe(false);
     expect(wrapper.find("#user-1").exists()).toBe(false);
-  });
-  test("When the SettingsForm has two Customer ID and two User ID per Customer ID in the Vuex, Then validate that dropDown of user is update with relevant values when we toggle between 'Customer account -1' to 'Customer account -2", async () => {
-    store.commit("settings/set_customer_account_ids", array_of_customer_ids);
-
-    wrapper = mount(ComponentToTest, {
-      store,
-      localVue,
-    });
-    expect(wrapper.vm.customers_options).toHaveLength(2);
-    expect(wrapper.find("#cust-0").text()).toStrictEqual("Customer account -1");
-    expect(wrapper.find("#cust-1").text()).toStrictEqual("Customer account -2");
-
-    await wrapper
-      .find("#input-dropdown-widget-cust-")
-      .setValue("Customer account -2");
-    expect(wrapper.find("#user-0").text()).toStrictEqual("Lab User  -1");
-    expect(wrapper.find("#user-1").text()).toStrictEqual("Intern -1");
-
-    await wrapper
-      .find("#input-dropdown-widget-cust-")
-      .setValue("Customer account -1");
-
-    expect(wrapper.find("#user-0").text()).toStrictEqual("User account -1");
-    expect(wrapper.find("#user-1").text()).toStrictEqual("User account -2");
   });
 });
