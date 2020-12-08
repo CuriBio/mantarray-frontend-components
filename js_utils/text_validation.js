59f760d0aff2a8dafefbbfc2af5fa45fa3fe06d1
raghu-at-750030001816 <raghu@nanosurfacebio.com>
2020-12-08 17:02:17 +0000

    Handling the text with null in the barcode

--- js_utils/text_validation.js
+++ js_utils/text_validation.js
@@ -69,6 +69,10 @@ export class TextValidation {
   validate_plate_barcode(text) {
     // refactored for 100% code coverage on JEST Unit testcases.
     let response = " "; // let us first assume that incoming text is invalid unless it passes the criteria.
+    // As the validation rules are on Vuex data of barcode and this can be null a check is required.
+    if (text == null) {
+      return response;
+    }
     const platebarcode_len = text.length;
     if (platebarcode_len < 10) {
       // clearly there is no need to process as its not matching basic rule length (< 10)
