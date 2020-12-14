c567239b6850d81d44cc4f5a71cf3f7b3165894e
raghu-at-750030001816 <raghu@nanosurfacebio.com>
2020-12-14 12:10:02 +0000

    Including and E2E testing to display StatusBar with ErrorCatchWidget

---
+++ pages/status/basic-error-catch.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="top: 111px; position: absolute">
+    <ComponentToTest></ComponentToTest>
+  </div>
+</template>
+
+<script>
+// import ComponentToTest from "@/components/status/StatusBar.vue";
+import { StatusBar as ComponentToTest } from "../../dist/mantarray.common";
+
+import { STATUS } from "@/store/modules/flask/enums";
+
+export default {
+  components: {
+    ComponentToTest,
+  },
+  created: function () {
+    this.$store.commit(
+      "flask/set_status_uuid",
+      STATUS.MESSAGE.CALIBRATION_NEEDED
+    );
+    setTimeout(this.reset_error, 10);
+  },
+  methods: {
+    reset_error: function () {
+      this.$store.commit("flask/set_status_uuid", STATUS.MESSAGE.ERROR);
+    },
+  },
+};
+</script>
--- tests/e2e/status/basic.e2e.js
+++ tests/e2e/status/basic.e2e.js
@@ -1,10 +1,17 @@
 import { Selector, ClientFunction } from "testcafe";
+import { RequestMock } from "testcafe";
 const CDP = require("chrome-remote-interface");
 const fs = require("fs");
 import { getPerformanceMetrics } from "@platform-os/testcafe-helpers";
 const path = require("path");
 
 import { testcafe_page_visual_regression } from "@curi-bio/frontend-test-utils";
+
+const shutdown_request_url = "http://localhost:4567/shutdown";
+
+const mocked_shutdown_commands = RequestMock()
+  .onRequestTo(shutdown_request_url)
+  .respond({}, 200, { "Access-Control-Allow-Origin": "*" });
 
 import VueSelector from "testcafe-vue-selectors";
 
@@ -14,6 +21,17 @@ fixture`status/basic`.page // declare the fixture
 
 test("status widget looks as expected with default values", async (t) => {
   const screenshot_path = path.join("status", "basic");
+
+  await testcafe_page_visual_regression(t, screenshot_path);
+});
+
+// the fixture declares what we are testing
+fixture`status/basic-error-catch`
+  .page(`http://localhost:8080/status/basic-error-catch`)
+  .requestHooks(mocked_shutdown_commands); // specify the start page
+
+test("status widget captures an ERROR Catch Widget", async (t) => {
+  const screenshot_path = path.join("status", "basic-status-error-catch");
 
   await testcafe_page_visual_regression(t, screenshot_path);
 });
