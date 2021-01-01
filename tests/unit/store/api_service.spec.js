13366d6c098f55eb8c9f6e761f353b6ce5db5d9c
raghu-at-750030001816 <raghu@nanosurfacebio.com>
2021-01-01 13:06:12 +0000

    Eslint rules enabled for api_service.spec.js

--- tests/unit/store/api_service.spec.js
+++ tests/unit/store/api_service.spec.js
@@ -1,54 +1,34 @@
 import Vue from "vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
-
 import { get_recording } from "@/store/ApiService.js";
 
-describe("api to get_recording", () => {
+describe("APIService", () => {
   beforeEach(() => {
     // jest.resetModules()
     // jest.clearAllMocks()
     jest.restoreAllMocks();
   });
 
-  it("Calls axios.get with the correct endpoint URL and returns the result of the HTTP request", async () => {
+  test("Given the endpoint URL is valid, When Axios does a get method call which is mocked, Then assert the value matches the mocked json object", async () => {
     const mocked_axios_get = jest.spyOn(Vue.axios, "get");
     mocked_axios_get.mockReturnValue(Promise.resolve({ data: 3 }));
 
-    // let mocked_axios_get = jest.fn();
-    // mocked_axios_get.mockImplementation(() => Promise.resolve({ data: 3 }));
-    // Vue.axios.get = mocked_axios_get;
-
     const result = await get_recording("sandbox_eli_waveform");
-    expect(Vue.axios.get).toBeCalledWith(
+    expect(Vue.axios.get).toHaveBeenCalledWith(
       "https://94fjmm5591.execute-api.us-east-1.amazonaws.com/sandbox_eli_waveform"
     );
-
-    expect(result).toEqual({ data: 3 });
-
-    //mocked_axios_get = null;
-    // Vue.axios.get = null
+    expect(result).toStrictEqual({ data: 3 });
   }),
-    it("Calls axios real API to obtain the waveform data from the HTTP GET", async () => {
-      //const data = {}
+    test("Given the endpoint URL is valid, When Axios obtains the response waveform data from the get method call, Then assert the value of the parameter total_recording_length", async () => {
+      // const data = {}
       jest.setTimeout(10000);
       const result = await get_recording("sandbox_eli_waveform");
-
-      //console.log(result)
-
-      // expect(Vue.axios.get).toBeCalledWith('https://94fjmm5591.execute-api.us-east-1.amazonaws.com/sandbox_eli_waveform')
-
-      expect(result.data["total_recording_length"]).toEqual(11200000);
-
-      //expect(result).toEqual(Promise.resolve(data))
+      expect(result.data["total_recording_length"]).toStrictEqual(11200000);
     }),
-    it("Calls axios real API to obtain the raw waveform data from the HTTP GET and extracts x_data_points and y_data_points", async () => {
+    test("Given the endpoint URL is valid, When Axios obtains the response waveform data from the get method call, Then extracts x_data_points and y_data_points and assert the value", async () => {
       const result = await get_recording("sandbox_eli_waveform");
-
-      var first_simple_json_waveform = result.data["waveform_data_points"][0];
-
-      expect(first_simple_json_waveform["x_data_points"][0]).toEqual(0);
-      expect(first_simple_json_waveform["y_data_points"][0]).toEqual(
+      const first_simple_json_waveform = result.data["waveform_data_points"][0];
+      expect(first_simple_json_waveform["x_data_points"][0]).toStrictEqual(0);
+      expect(first_simple_json_waveform["y_data_points"][0]).toStrictEqual(
         230.417297
       );
     });
