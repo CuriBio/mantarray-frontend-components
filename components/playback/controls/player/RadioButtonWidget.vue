dd83fcca909b6133ab8b83f916448d9c2121703a
raghu-at-750030001816 <raghu@nanosurfacebio.com>
2021-01-06 18:41:51 +0000

    Basic Setup of RadioButtonWidget Component

---
+++ components/playback/controls/player/RadioButtonWidget.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div
+      id="cmpD488e8d876c21c942a854674129609c77_item0"
+      data-cls="cmpD488e8d876c21c942a854674129609c77_item0_cls"
+      class="cmpD488e8d876c21c942a854674129609c77_itemCls cmpD488e8d876c21c942a854674129609c77_item0_cls"
+      style="
+        white-space: nowrap;
+        padding-top: 15px;
+        color: #b7b7b7;
+        padding-bottom: 7px;
+      "
+    >
+      <i
+        id="cmpD488e8d876c21c942a854674129609c77_item0_dd"
+        style="
+          display: inline-block;
+          width: 22.5px;
+          vertical-align: middle;
+          line-height: inherit;
+          margin-top: -1px;
+          font-size: 22.5px;
+        "
+        class="fa fa-dot-circle-o"
+      ></i>
+      Spike&nbsp;Amplitude
+    </div>
+  </div>
+</template>
+<script>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCircle as fa_circle } from "@fortawesome/free-solid-svg-icons";
+import { faDotCircle as fa_dotcircle } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fa_circle);
+library.add(fa_dotcircle);
+export default {
+  name: "RadioButtonWidget",
+  //   components: {
+  //     FontAwesomeIcon,
+  //   },
+};
+</script>
+<style type="text/css"></style>
