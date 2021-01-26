5b631db0c0a6c7cacb65a2c55461fe4208e8ac4f
raghu-at-750030001816 <raghu@nanosurfacebio.com>
2021-01-26 15:58:10 +0000

    Intermediate Stage of SimulationStudio

--- components/playback/controls/SimulationStudioWidget.vue
+++ components/playback/controls/SimulationStudioWidget.vue
@@ -1,27 +1,7 @@
 <template>
   <div>
-    <div
-      id="cmpD1d83165d7321a67ce408a4ab3362bc9e"
-      class="mfWFCompCls"
-      style="
-        transform: rotate(0deg);
-        box-sizing: border-box;
-        padding: 0px;
-        margin: 0px;
-        background: rgb(28, 28, 28);
-        position: absolute;
-        width: 415px;
-        height: 280px;
-        top: 94px;
-        left: 737.5px;
-        visibility: visible;
-        border: 0px none rgb(0, 0, 0);
-        border-radius: 10px;
-        box-shadow: rgba(0, 0, 0, 0.7) 0px 0px 10px 0px;
-        z-index: 7;
-        pointer-events: all;
-      "
-    ></div>
+    <!-- original mockflow ID: id="cmpD1d83165d7321a67ce408a4ab3362bc9e" -->
+    <div class="div__simulationstudio-backdrop"></div>
     <div
       id="cmpDab1a64dec495767a5b74b08ff7a4e5b9"
       class="mfWFCompCls"
@@ -406,6 +386,750 @@
       "
       >A</span
     >
+    <div
+      id="cmpDbd8878d603962e4ba7ef88f8b0a71104"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 119.427px;
+        left: 829.084px;
+        visibility: visible;
+        z-index: 11;
+      "
+    >
+      <PlateWell
+        :classname="'plate_1'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#19AC8A'"
+        :stroke_wdth="4"
+        :index="1"
+      ></PlateWell>
+    </div>
+    <span
+      id="cmpD5a298209bb566d2bf2eccf5e082447df"
+      class="mfWFCompCls"
+      style="
+        pointer-events: all;
+        line-height: 100%;
+        transform: rotate(0deg);
+        overflow: hidden;
+        position: absolute;
+        width: 25px;
+        height: 25px;
+        top: 137px;
+        left: 852px;
+        padding: 5px;
+        visibility: visible;
+        user-select: none;
+        font-family: Muli;
+        font-weight: bold;
+        font-style: normal;
+        text-decoration: none;
+        font-size: 20px;
+        color: rgb(255, 255, 255);
+        text-align: left;
+        z-index: 103;
+      "
+      >A</span
+    >
+    <div
+      id="cmpD95bb5025d2b2a6ad5d556fa77ecd9fb6"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 119.427px;
+        left: 890.688px;
+        visibility: visible;
+        z-index: 13;
+      "
+    >
+      <PlateWell
+        :classname="'plate_2'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#19AC8A'"
+        :stroke_wdth="4"
+        :index="2"
+      ></PlateWell>
+    </div>
+    <span
+      id="cmpD53be34d08254a0ca47ecbb06296f68a8"
+      class="mfWFCompCls"
+      style="
+        pointer-events: all;
+        line-height: 100%;
+        transform: rotate(0deg);
+        overflow: hidden;
+        position: absolute;
+        width: 25px;
+        height: 25px;
+        top: 137px;
+        left: 914px;
+        padding: 5px;
+        visibility: visible;
+        user-select: none;
+        font-family: Muli;
+        font-weight: bold;
+        font-style: normal;
+        text-decoration: none;
+        font-size: 20px;
+        color: rgb(255, 255, 255);
+        text-align: left;
+        z-index: 105;
+      "
+      >B</span
+    >
+    <div
+      id="cmpD5c3527dd3dbecf163acb3283d6407496"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 119.427px;
+        left: 952.292px;
+        visibility: visible;
+        z-index: 15;
+      "
+    >
+      <PlateWell
+        :classname="'plate_3'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="3"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpDc680429ca0a8c56f1514d8d454fd76b4"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 119.427px;
+        left: 1013.9px;
+        visibility: visible;
+        z-index: 17;
+      "
+    >
+      <PlateWell
+        :classname="'plate_4'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="4"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpDf824bd01cb8921eb6679f20c34f17ae4"
+      class="mfWFCompCls"
+      width="64"
+      height="64"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 64px;
+        height: 64px;
+        top: 119.992px;
+        left: 1076.5px;
+        visibility: visible;
+        z-index: 87;
+      "
+    >
+      <PlateWell
+        :classname="'plate_5'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="5"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpD3fbf82b96ba1e9ce72155cada4fc8a83"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 179.352px;
+        left: 767.479px;
+        visibility: visible;
+        z-index: 19;
+      "
+    >
+      <PlateWell
+        :classname="'plate_6'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="6"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpD02af26a4f10d65b9e8e90fd6488cf379"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 179.352px;
+        left: 829.084px;
+        visibility: visible;
+        z-index: 21;
+      "
+    >
+      <PlateWell
+        :classname="'plate_7'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="7"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpDbda73598c13e8d1cb8a955c8cca99679"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 179.352px;
+        left: 890.688px;
+        visibility: visible;
+        z-index: 23;
+      "
+    >
+      <PlateWell
+        :classname="'plate_8'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="8"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpD2557f27c702e01f9903f46a1a0b39d9b"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 179.352px;
+        left: 952.292px;
+        visibility: visible;
+        z-index: 25;
+      "
+    >
+      <PlateWell
+        :classname="'plate_9'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="9"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpD6d28fb7c7b3f10d8133f9e8f6e183970"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 179.352px;
+        left: 1013.9px;
+        visibility: visible;
+        z-index: 27;
+      "
+    >
+      <PlateWell
+        :classname="'plate_10'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="10"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpDbc499ed7c179685301ef8cbde86d5ebc"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 179.352px;
+        left: 1075.5px;
+        visibility: visible;
+        z-index: 29;
+      "
+    >
+      <PlateWell
+        :classname="'plate_11'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="11"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpD0959b9c2a8d6c2c9cea3880e25b3269e"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 239.278px;
+        left: 767.479px;
+        visibility: visible;
+        z-index: 31;
+      "
+    >
+      <PlateWell
+        :classname="'plate_12'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="12"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpD37f37409867ab94ded6091c26b0b1690"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 239.278px;
+        left: 829.084px;
+        visibility: visible;
+        z-index: 33;
+      "
+    >
+      <PlateWell
+        :classname="'plate_13'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="13"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpD4c7dcc2a9caab65555dff4890816b811"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 239.278px;
+        left: 890.688px;
+        visibility: visible;
+        z-index: 35;
+      "
+    >
+      <PlateWell
+        :classname="'plate_14'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="14"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpDf6f7e88135857174a5b423d6f5c6015e"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 239.278px;
+        left: 952.292px;
+        visibility: visible;
+        z-index: 37;
+      "
+    >
+      <PlateWell
+        :classname="'plate_15'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="15"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpD15d70aa3324fc5ceace7904234dbbba5"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 239.278px;
+        left: 1013.9px;
+        visibility: visible;
+        z-index: 39;
+      "
+    >
+      <PlateWell
+        :classname="'plate_16'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="16"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpD032a9777a1b2b69dd5319852a34474e9"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 239.278px;
+        left: 1075.5px;
+        visibility: visible;
+        z-index: 41;
+      "
+    >
+      <PlateWell
+        :classname="'plate_17'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="17"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpD320608a6ca596f39b31c8dae1b9bc1a8"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 299.157px;
+        left: 767.479px;
+        visibility: visible;
+        z-index: 43;
+      "
+    >
+      <PlateWell
+        :classname="'plate_18'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="18"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpDa42d5045b1cac30acfb30ed673e6219d"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 299.203px;
+        left: 829.084px;
+        visibility: visible;
+        z-index: 45;
+      "
+    >
+      <PlateWell
+        :classname="'plate_19'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="19"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpDc27ccb4aa19e9b3afff73e688924132d"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 299.203px;
+        left: 890.688px;
+        visibility: visible;
+        z-index: 47;
+      "
+    >
+      <PlateWell
+        :classname="'plate_20'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="20"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpD8a9ecf6b662b877dc53fc21aea6adbac"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 299.203px;
+        left: 952.292px;
+        visibility: visible;
+        z-index: 49;
+      "
+    >
+      <PlateWell
+        :classname="'plate_21'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="21"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpDa76d35a54ecbf373fd350d2a5ab020eb"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 299.203px;
+        left: 1013.9px;
+        visibility: visible;
+        z-index: 51;
+      "
+    >
+      <PlateWell
+        :classname="'plate_22'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="22"
+      ></PlateWell>
+    </div>
+    <div
+      id="cmpDab5a3edfa307390da919175d89ea498a"
+      class="mfWFCompCls"
+      width="66"
+      height="66"
+      style="
+        pointer-events: all;
+        transform: rotate(0deg);
+        position: absolute;
+        width: 66px;
+        height: 66px;
+        top: 299.203px;
+        left: 1075.5px;
+        visibility: visible;
+        z-index: 53;
+      "
+    >
+      <PlateWell
+        :classname="'plate_23'"
+        :svg_height="72"
+        :svg_width="72"
+        :circle_x="36"
+        :circle_y="36"
+        :radius="28"
+        :strk="'#1C1C1C'"
+        :plate_fill="'#B7B7B7'"
+        :stroke_wdth="4"
+        :index="23"
+      ></PlateWell>
+    </div>
   </div>
 </template>
 <script>
@@ -426,4 +1150,23 @@ export default {
   methods: {},
 };
 </script>
-<style></style>
+<style>
+.div__simulationstudio-backdrop {
+  transform: rotate(0deg);
+  box-sizing: border-box;
+  padding: 0px;
+  margin: 0px;
+  background: rgb(28, 28, 28);
+  position: absolute;
+  width: 415px;
+  height: 280px;
+  top: 94px;
+  left: 737.5px;
+  visibility: visible;
+  border: 0px none rgb(0, 0, 0);
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.7) 0px 0px 10px 0px;
+  z-index: 7;
+  pointer-events: all;
+}
+</style>
