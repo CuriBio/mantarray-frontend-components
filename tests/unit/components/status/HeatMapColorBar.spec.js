99aba37802135a84f365f820904ad6f19dccff97
raghu-at-750030001816 <raghu@nanosurfacebio.com>
2021-02-23 04:32:00 +0000

    Resolving all the issues of <wbr> and disabling prettier

--- tests/unit/components/status/HeatMapColorBar.spec.js
+++ tests/unit/components/status/HeatMapColorBar.spec.js
@@ -12,7 +12,7 @@ const localVue = createLocalVue();
 
 describe("HeatMapColorBar.vue", () => {
   // afterEach(() => wrapper.destroy());
-  test("When mounting HeatMapColorBar from the build dist file, Then it loads successfully and higher_range text as defined rendered", () => {
+  test("When mounting HeatMapColorBar from the build dist file, Then it loads successfully and upper_range text '100 μN' is rendered as defined", () => {
     const propsData = {
       upper_range: "100",
     };
@@ -23,7 +23,7 @@ describe("HeatMapColorBar.vue", () => {
     const target_span = wrapper.find(".span__heatmap-scale-higher-value");
     expect(target_span.text()).toStrictEqual("100 μN");
   });
-  test("Given that the uuid, upper_range, lower_range and height, When mounted successfully, Then assert the default linear-gradient,upper_range,lower_range and height are applied on HeatMapColorBar", async () => {
+  test("Given that the uuid, upper_range, lower_range and height, When mounted successfully, Then assert that upper_range,lower_range,height are applied and as uuid is match a default linear-gradient is applied on HeatMapColorBar", async () => {
     const propsData = {
       upper_range: "100",
       lower_range: "0",
@@ -37,9 +37,9 @@ describe("HeatMapColorBar.vue", () => {
     const target_span_higher = wrapper.find(
       ".span__heatmap-scale-higher-value"
     );
-    expect(target_span_higher.text()).toStrictEqual("100 μN");
+    expect(target_span_higher.text()).toStrictEqual("100 μN");
     const target_span_lower = wrapper.find(".span__heatmap-scale-lower-value");
-    expect(target_span_lower.text()).toStrictEqual("0 μN");
+    expect(target_span_lower.text()).toStrictEqual("0 μN");
 
     const target_div_gradient_holder = wrapper.find(
       ".div__heatmap-gradient-holder"
@@ -53,10 +53,10 @@ describe("HeatMapColorBar.vue", () => {
       localVue,
     });
     expect(renderwrapper).toContain(
-      '<div data-server-rendered="true"><div class="div__heatmap-gradient-holder" style="height:481px;background:linear-gradient(to top,  #2c7bb6 0%,#00a6ca 12.5%,#00ccbc 25%,#90eb9d 37.5%,#ffff8c 50%,#f9d057 62.5%,#f29e2e 75%,#e76818 87.5%,#d7191c 100%);"></div> <span class="span__heatmap-scale-higher-value"> 100 <wbr>μN</span> <span class="span__heatmap-scale-lower-value" style="top:465px;">0 <wbr>μN</span></div>'
+      '<div data-server-rendered="true"><div class="div__heatmap-gradient-holder" style="height:481px;background:linear-gradient(to top,  #2c7bb6 0%,#00a6ca 12.5%,#00ccbc 25%,#90eb9d 37.5%,#ffff8c 50%,#f9d057 62.5%,#f29e2e 75%,#e76818 87.5%,#d7191c 100%);"></div> <span class="span__heatmap-scale-higher-value">100 μN</span> <span class="span__heatmap-scale-lower-value" style="top:465px;">0 μN</span></div>'
     );
   });
-  test("Given that the uuid is empty, upper_range, lower_range and height, When mounted successfully, Then assert the default linear-gradient,upper_range,lower_range and height are applied on HeatMapColorBar", async () => {
+  test("Given that the uuid is empty, upper_range, lower_range, height, and gradient_range, When mounted successfully, Then assert that upper_range,lower_range, height are applied on HeatMapColorBar and as uuid is empty then gradient_range values are applied on linear-gradient", async () => {
     const propsData = {
       upper_range: "100",
       lower_range: "0",
@@ -78,9 +78,9 @@ describe("HeatMapColorBar.vue", () => {
     const target_span_higher = wrapper.find(
       ".span__heatmap-scale-higher-value"
     );
-    expect(target_span_higher.text()).toStrictEqual("100 μN");
+    expect(target_span_higher.text()).toStrictEqual("100 μN");
     const target_span_lower = wrapper.find(".span__heatmap-scale-lower-value");
-    expect(target_span_lower.text()).toStrictEqual("0 μN");
+    expect(target_span_lower.text()).toStrictEqual("0 μN");
 
     const target_div_gradient_holder = wrapper.find(
       ".div__heatmap-gradient-holder"
@@ -94,11 +94,11 @@ describe("HeatMapColorBar.vue", () => {
       localVue,
     });
     expect(renderwrapper).toContain(
-      '<div data-server-rendered="true"><div class="div__heatmap-gradient-holder" style="height:481px;background:linear-gradient(to top, #2c7bb6 0%,#00a6ca 25%,#00ccbc 50%,#90eb9d 75%,#ffff8c 100%);"></div> <span class="span__heatmap-scale-higher-value"> 100 <wbr>μN</span> <span class="span__heatmap-scale-lower-value" style="top:465px;">0 <wbr>μN</span></div>'
+      '<div data-server-rendered="true"><div class="div__heatmap-gradient-holder" style="height:481px;background:linear-gradient(to top, #2c7bb6 0%,#00a6ca 25%,#00ccbc 50%,#90eb9d 75%,#ffff8c 100%);"></div> <span class="span__heatmap-scale-higher-value">100 μN</span> <span class="span__heatmap-scale-lower-value" style="top:465px;">0 μN</span></div>'
     );
   });
 
-  test("Given that the uuid, upper_range, lower_range and height, When mounted successfully, Then update the heatmap_height to 400, assert the default linear-gradient,upper_range,lower_range and height applied on HeatMapColorBar, observe the height is updated to 400", async () => {
+  test("Given that the uuid, upper_range, lower_range and height, When mounted successfully, Then prop update of the heatmap_height to 400, assert height is updated to 400 and as uuid is match a default linear-gradient is applied on HeatMapColorBar", async () => {
     const propsData = {
       upper_range: "100",
       lower_range: "0",
@@ -109,20 +109,19 @@ describe("HeatMapColorBar.vue", () => {
       propsData,
       localVue,
     });
-    const target_span_higher = wrapper.find(
-      ".span__heatmap-scale-higher-value"
+
+    const target_div_gradient_holder = wrapper.find(
+      ".div__heatmap-gradient-holder"
     );
-    expect(target_span_higher.text()).toStrictEqual("100 μN");
-    const target_span_lower = wrapper.find(".span__heatmap-scale-lower-value");
-    expect(target_span_lower.text()).toStrictEqual("0 μN");
+
+    expect(target_div_gradient_holder.attributes().style).toBe(
+      "height: 481px;"
+    );
 
     await wrapper.setProps({ heatmap_height: 400 });
 
     await wrapper.vm.$nextTick();
 
-    const target_div_gradient_holder = wrapper.find(
-      ".div__heatmap-gradient-holder"
-    );
     expect(target_div_gradient_holder.attributes().style).toBe(
       "height: 400px;"
     );
@@ -134,10 +133,10 @@ describe("HeatMapColorBar.vue", () => {
       localVue,
     });
     expect(renderwrapper).toContain(
-      '<div data-server-rendered="true"><div class="div__heatmap-gradient-holder" style="height:400px;background:linear-gradient(to top,  #2c7bb6 0%,#00a6ca 12.5%,#00ccbc 25%,#90eb9d 37.5%,#ffff8c 50%,#f9d057 62.5%,#f29e2e 75%,#e76818 87.5%,#d7191c 100%);"></div> <span class="span__heatmap-scale-higher-value"> 100 <wbr>μN</span> <span class="span__heatmap-scale-lower-value" style="top:384px;">0 <wbr>μN</span></div>'
+      '<div data-server-rendered="true"><div class="div__heatmap-gradient-holder" style="height:400px;background:linear-gradient(to top,  #2c7bb6 0%,#00a6ca 12.5%,#00ccbc 25%,#90eb9d 37.5%,#ffff8c 50%,#f9d057 62.5%,#f29e2e 75%,#e76818 87.5%,#d7191c 100%);"></div> <span class="span__heatmap-scale-higher-value">100 μN</span> <span class="span__heatmap-scale-lower-value" style="top:384px;">0 μN</span></div>'
     );
   });
-  test("Given that the uuid is null, upper_range, lower_range and height, When mounted successfully, Then assert the default linear-gradient,upper_range,lower_range and height are applied on HeatMapColorBar", async () => {
+  test("Given that the uuid is null, upper_range, lower_range and height, When mounted successfully, Then prop update of the heatmap_height to 400, assert height is updated to 400 and as uuid is empty then gradient_range values are applied on linear-gradient", async () => {
     const propsData = {
       upper_range: "100",
       lower_range: "0",
@@ -156,16 +155,11 @@ describe("HeatMapColorBar.vue", () => {
       propsData,
       localVue,
     });
-    const target_span_higher = wrapper.find(
-      ".span__heatmap-scale-higher-value"
-    );
-    expect(target_span_higher.text()).toStrictEqual("100 μN");
-    const target_span_lower = wrapper.find(".span__heatmap-scale-lower-value");
-    expect(target_span_lower.text()).toStrictEqual("0 μN");
 
     const target_div_gradient_holder = wrapper.find(
       ".div__heatmap-gradient-holder"
     );
+
     expect(target_div_gradient_holder.attributes().style).toBe(
       "height: 481px;"
     );
@@ -186,11 +180,11 @@ describe("HeatMapColorBar.vue", () => {
     });
 
     expect(renderwrapper).toContain(
-      '<div data-server-rendered="true"><div class="div__heatmap-gradient-holder" style="height:400px;background:linear-gradient(to top, #2c7bb6 0%,#00a6ca 25%,#00ccbc 50%,#90eb9d 75%,#ffff8c 100%);"></div> <span class="span__heatmap-scale-higher-value"> 100 <wbr>μN</span> <span class="span__heatmap-scale-lower-value" style="top:384px;">0 <wbr>μN</span></div>'
+      '<div data-server-rendered="true"><div class="div__heatmap-gradient-holder" style="height:400px;background:linear-gradient(to top, #2c7bb6 0%,#00a6ca 25%,#00ccbc 50%,#90eb9d 75%,#ffff8c 100%);"></div> <span class="span__heatmap-scale-higher-value">100 μN</span> <span class="span__heatmap-scale-lower-value" style="top:384px;">0 μN</span></div>'
     );
   });
 
-  test("Given that the uuid is empty, upper_range, lower_range and height, When mounted successfully, Then update the uuid to valid value, assert the default linear-gradient,upper_range,lower_range and height applied on HeatMapColorBar, observe that the linear_gradient is updated to provided value", async () => {
+  test("Given that the uuid is empty, upper_range, lower_range and height, When mounted successfully, Then update the uuid to valid value, and as uuid is match a default linear-gradient is applied on HeatMapColorBar", async () => {
     const propsData = {
       upper_range: "100",
       lower_range: "0",
@@ -208,12 +202,6 @@ describe("HeatMapColorBar.vue", () => {
       propsData,
       localVue,
     });
-    const target_span_higher = wrapper.find(
-      ".span__heatmap-scale-higher-value"
-    );
-    expect(target_span_higher.text()).toStrictEqual("100 μN");
-    const target_span_lower = wrapper.find(".span__heatmap-scale-lower-value");
-    expect(target_span_lower.text()).toStrictEqual("0 μN");
 
     await wrapper.setProps({
       gradient_uuid: "0f81155d-23ec-4790-a3fe-92a6cc7c3c47",
@@ -226,10 +214,10 @@ describe("HeatMapColorBar.vue", () => {
       localVue,
     });
     expect(renderwrapper).toContain(
-      '<div data-server-rendered="true"><div class="div__heatmap-gradient-holder" style="height:481px;background:linear-gradient(to top,  #2c7bb6 0%,#00a6ca 12.5%,#00ccbc 25%,#90eb9d 37.5%,#ffff8c 50%,#f9d057 62.5%,#f29e2e 75%,#e76818 87.5%,#d7191c 100%);"></div> <span class="span__heatmap-scale-higher-value"> 100 <wbr>μN</span> <span class="span__heatmap-scale-lower-value" style="top:465px;">0 <wbr>μN</span></div>'
+      '<div data-server-rendered="true"><div class="div__heatmap-gradient-holder" style="height:481px;background:linear-gradient(to top,  #2c7bb6 0%,#00a6ca 12.5%,#00ccbc 25%,#90eb9d 37.5%,#ffff8c 50%,#f9d057 62.5%,#f29e2e 75%,#e76818 87.5%,#d7191c 100%);"></div> <span class="span__heatmap-scale-higher-value">100 μN</span> <span class="span__heatmap-scale-lower-value" style="top:465px;">0 μN</span></div>'
     );
   });
-  test("Given that the uuid is empty, upper_range, lower_range, gradient_range and height, When mounted successfully, Then update the gradient_range to different range, assert the default linear-gradient,upper_range,lower_range and height applied on HeatMapColorBar, observe that the linear_gradient is updated to provided value", async () => {
+  test("Given that the uuid is empty, upper_range, lower_range, gradient_range and height, When mounted successfully, Then update the gradient_range to different range, and assert linear-gradient as per the updated gradient_range on HeatMapColorBar", async () => {
     const propsData = {
       upper_range: "100",
       lower_range: "0",
@@ -250,9 +238,9 @@ describe("HeatMapColorBar.vue", () => {
     const target_span_higher = wrapper.find(
       ".span__heatmap-scale-higher-value"
     );
-    expect(target_span_higher.text()).toStrictEqual("100 μN");
+    expect(target_span_higher.text()).toStrictEqual("100 μN");
     const target_span_lower = wrapper.find(".span__heatmap-scale-lower-value");
-    expect(target_span_lower.text()).toStrictEqual("0 μN");
+    expect(target_span_lower.text()).toStrictEqual("0 μN");
 
     await wrapper.setProps({
       gradient_range: [
@@ -284,7 +272,7 @@ describe("HeatMapColorBar.vue", () => {
       localVue,
     });
     expect(renderwrapper).toContain(
-      '<div data-server-rendered="true"><div class="div__heatmap-gradient-holder" style="height:481px;background:linear-gradient(to top, #2c7bb6 0%,#00a6ca 12.5%,#00ccbc 25%,#90eb9d 37.5%,#ffff8c 50%,#f9d057 62.5%,#f29e2e 75%,#e76818 87.5%,#d7191c 100%);"></div> <span class="span__heatmap-scale-higher-value"> 100 <wbr>μN</span> <span class="span__heatmap-scale-lower-value" style="top:465px;">0 <wbr>μN</span></div>'
+      '<div data-server-rendered="true"><div class="div__heatmap-gradient-holder" style="height:481px;background:linear-gradient(to top, #2c7bb6 0%,#00a6ca 12.5%,#00ccbc 25%,#90eb9d 37.5%,#ffff8c 50%,#f9d057 62.5%,#f29e2e 75%,#e76818 87.5%,#d7191c 100%);"></div> <span class="span__heatmap-scale-higher-value">100 μN</span> <span class="span__heatmap-scale-lower-value" style="top:465px;">0 μN</span></div>'
     );
   });
 });
