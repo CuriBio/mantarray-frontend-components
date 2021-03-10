9ecf4a4b5f39fb7925d6f752314302e946876012
raghu-at-750030001816 <raghu@nanosurfacebio.com>
2021-03-10 15:33:25 +0000

    Introducing all forms of JEST testcases to complete click, click+ctrl and click+shift on (+) or (-) icons, Rows A, B, C, D and Columns 1, 2, 3, 4, 5, 6

--- tests/unit/components/plate_based_widgets/mapeditor/PlateMapRowColumn.spec.js
+++ tests/unit/components/plate_based_widgets/mapeditor/PlateMapRowColumn.spec.js
@@ -242,6 +242,90 @@ describe("PlateMapEditor.vue", () => {
       ".span__platemap-editor-column-index-six > label",
     ],
   ])(
+    "Given that none of the wells are selected, When user Ctrl+clicks the Row/Column button, Then wells corresponding to that row is seleced with outline of 4px;",
+    async (string_name_of_button, array_of_well_indices, selector_str) => {
+      const select = [];
+      for (let i = 0; i < 24; i++) {
+        select.push(false);
+      }
+      const color = new Array(24).fill("#b7b7b7");
+
+      const propsData = {
+        selected: select,
+        platecolor: color,
+      };
+      wrapper = mount(ComponentToTest, {
+        propsData,
+        store,
+        localVue,
+      });
+
+      const selector = wrapper.find(selector_str);
+
+      await selector.trigger("click", {
+        ctrlKey: true, // For testing @click.shift handlers
+      });
+      await wrapper.vm.$nextTick(); // wait for update
+
+      for (let count = 0; count < array_of_well_indices.length; count++) {
+        const well_index = array_of_well_indices[count];
+        const well = wrapper.find(".plate_" + well_index);
+        expect(well.attributes("stroke-width")).toBe("4");
+      }
+    }
+  );
+  test.each([
+    [
+      "Row A",
+      [0, 4, 8, 12, 16, 20],
+      ".span__platemap-editor-row-index-A > label",
+    ],
+    [
+      "Row B",
+      [1, 5, 9, 13, 17, 21],
+      ".span__platemap-editor-row-index-B > label",
+    ],
+    [
+      "Row C",
+      [2, 6, 10, 14, 18, 22],
+      ".span__platemap-editor-row-index-C > label",
+    ],
+    [
+      "Row D",
+      [3, 7, 11, 15, 19, 23],
+      ".span__platemap-editor-row-index-D > label",
+    ],
+    [
+      "Column 01",
+      [0, 1, 2, 3],
+      ".span__platemap-editor-column-index-one > label",
+    ],
+    [
+      "Column 02",
+      [4, 5, 6, 7],
+      ".span__platemap-editor-column-index-two > label",
+    ],
+    [
+      "Column 03",
+      [8, 9, 10, 11],
+      ".span__platemap-editor-column-index-three > label",
+    ],
+    [
+      "Column 04",
+      [12, 13, 14, 15],
+      ".span__platemap-editor-column-index-four > label",
+    ],
+    [
+      "Column 05",
+      [16, 17, 18, 19],
+      ".span__platemap-editor-column-index-five > label",
+    ],
+    [
+      "Column 06",
+      [20, 21, 22, 23],
+      ".span__platemap-editor-column-index-six > label",
+    ],
+  ])(
     "Given that odd numbered  wells are selected, When user Shift+clicks the Row/Column button, Then wells corresponding to even numbered is seleced with outline of 4px;",
     async (string_name_of_button, array_of_well_indices, selector_str) => {
       const select = [];
@@ -370,6 +454,90 @@ describe("PlateMapEditor.vue", () => {
         const well_index = array_of_well_indices[count];
         const well = wrapper.find(".plate_" + well_index);
         expect(well.attributes("stroke-width")).toBe("4");
+      }
+    }
+  );
+  test.each([
+    [
+      "Row A",
+      [0, 4, 8, 12, 16, 20],
+      ".span__platemap-editor-row-index-A > label",
+    ],
+    [
+      "Row B",
+      [1, 5, 9, 13, 17, 21],
+      ".span__platemap-editor-row-index-B > label",
+    ],
+    [
+      "Row C",
+      [2, 6, 10, 14, 18, 22],
+      ".span__platemap-editor-row-index-C > label",
+    ],
+    [
+      "Row D",
+      [3, 7, 11, 15, 19, 23],
+      ".span__platemap-editor-row-index-D > label",
+    ],
+    [
+      "Column 01",
+      [0, 1, 2, 3],
+      ".span__platemap-editor-column-index-one > label",
+    ],
+    [
+      "Column 02",
+      [4, 5, 6, 7],
+      ".span__platemap-editor-column-index-two > label",
+    ],
+    [
+      "Column 03",
+      [8, 9, 10, 11],
+      ".span__platemap-editor-column-index-three > label",
+    ],
+    [
+      "Column 04",
+      [12, 13, 14, 15],
+      ".span__platemap-editor-column-index-four > label",
+    ],
+    [
+      "Column 05",
+      [16, 17, 18, 19],
+      ".span__platemap-editor-column-index-five > label",
+    ],
+    [
+      "Column 06",
+      [20, 21, 22, 23],
+      ".span__platemap-editor-column-index-six > label",
+    ],
+  ])(
+    "Given that all the wells are selected, When user Shift+clicks the Row/Column button, Then wells corresponding row/columns get deselected and with outline of 0px;",
+    async (string_name_of_button, array_of_well_indices, selector_str) => {
+      const select = [];
+      for (let i = 0; i < 24; i++) {
+        select.push(true);
+      }
+      const color = new Array(24).fill("#b7b7b7");
+
+      const propsData = {
+        selected: select,
+        platecolor: color,
+      };
+      wrapper = mount(ComponentToTest, {
+        propsData,
+        store,
+        localVue,
+      });
+
+      const selector = wrapper.find(selector_str);
+
+      await selector.trigger("click", {
+        shiftKey: true, // For testing @click.shift handlers
+      });
+      await wrapper.vm.$nextTick(); // wait for update
+
+      for (let count = 0; count < array_of_well_indices.length; count++) {
+        const well_index = array_of_well_indices[count];
+        const well = wrapper.find(".plate_" + well_index);
+        expect(well.attributes("stroke-width")).toBe("0");
       }
     }
   );
