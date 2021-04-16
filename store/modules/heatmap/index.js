4be74bf4c8fbccbb545178500ca19e7c2d49ad4a
raghu-at-750030001816 <raghu@nanosurfacebio.com>
2021-04-16 13:48:04 +0000

    Implementation of basic structure of heatmap Vuex Store

---
+++ store/modules/heatmap/index.js
@@ -0,0 +1,20 @@
+// adapted from https://stackoverflow.com/questions/53446792/nuxt-vuex-how-do-i-break-down-a-vuex-module-into-separate-files
+
+import getters from "./getters";
+import mutations from "./mutations";
+
+const default_state = {
+  // heatmap_values: 24 element array. each element corresponds to a well in the plate
+  heatmap_values: [],
+  heatmap_idx: null,
+};
+
+// adapted from https://itnext.io/eating-my-advice-efficiently-improving-on-understanding-and-using-nuxt-vuex-6d00769014a2
+const state = () => JSON.parse(JSON.stringify(default_state));
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  getters,
+};
---
+++ store/modules/heatmap/mutations.js
@@ -0,0 +1,9 @@
+export default {
+  set_heatmap_idx(state, new_value) {
+    state.heatmap_idx = new_value;
+  },
+
+  set_heatmap_values(state, new_value) {
+    state.heatmap_values = new_value;
+  },
+};
