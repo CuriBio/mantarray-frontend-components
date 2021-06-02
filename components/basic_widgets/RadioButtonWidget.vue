6e52898e8802dc1207920f3a77073ac31270b81b
Luci Pak <84344924+luciipak@users.noreply.github.com>
2021-06-02 16:12:51 -0700

    Update RadioButtonWidget.vue

--- components/basic_widgets/RadioButtonWidget.vue
+++ components/basic_widgets/RadioButtonWidget.vue
@@ -35,7 +35,7 @@ export default {
   },
 
   created: function () {
-    if (this.pre_selected !== undefined) {
+    if (this.pre_selected != undefined) {
       this.selected = this.radio_buttons[this.pre_selected];
     }
   },
