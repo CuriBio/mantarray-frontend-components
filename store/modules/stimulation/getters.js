77fb1f1bbb8108c79b3862b8af032c1301e5a6ea
Luci Pak <84344924+luciipak@users.noreply.github.com>
2021-07-14 15:57:30 -0700

    Update getters.js

--- store/modules/stimulation/getters.js
+++ store/modules/stimulation/getters.js
@@ -33,5 +33,5 @@ const get_new_protocol_letter = ({ protocol_list }) => {
   return alphabet[current_alphabet_idx + 1];
 };
 
-const alphabet = Array.from(Array(26).keys()).map(i => String.fromCharCode(65 + i));
+const alphabet = Array.from(Array(26).keys()).map((i) => String.fromCharCode(65 + i));
 
