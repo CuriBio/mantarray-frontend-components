84ca9803c7440cf654fed6c710295c349f3e5b44
Tanner Peterson <tannermp23@hotmail.com>
2021-07-19 09:45:45 -0700

    restoring StimulationStudioCreateAndEdit.spec.js

--- tests/unit/components/stimulation/StimulationStudioCreateAndEdit.spec.js
+++ tests/unit/components/stimulation/StimulationStudioCreateAndEdit.spec.js
@@ -17,14 +17,16 @@ describe("StimulationStudioCreateAndEdit.vue", () => {
     store = await NuxtStore.createStore();
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test("When mounting StimulationStudioCreateAndEdit from the component file, Then the correct number of protocols from state should appear in the drop down selection", () => {
     const wrapper = mount(StimulationStudioCreateAndEdit, {
       store,
       localVue,
     });
-    const labeled_protocols = store.state.stimulation.protocol_list.filter(
-      (protocol) => protocol.label.length != 0
-    ).length;
+    const labeled_protocols = store.state.stimulation.protocol_list.length - 1;
     const target_span = wrapper.findAll("li");
     expect(target_span).toHaveLength(labeled_protocols);
   });
@@ -36,8 +38,7 @@ describe("StimulationStudioCreateAndEdit.vue", () => {
     });
     await store.dispatch("stimulation/handle_selected_wells", [false, true, false, true]);
     const options = wrapper.findAll("li");
-    await options.at(1).trigger("click");
-    console.log(wrapper.vm.selected_protocol_idx);
+    await options.at(0).trigger("click");
     await wrapper.vm.handle_click(0);
     const expected_value = store.state.stimulation.protocol_list[wrapper.vm.selected_protocol_idx];
     store.state.stimulation.selected_wells.map((well) => {
@@ -52,11 +53,59 @@ describe("StimulationStudioCreateAndEdit.vue", () => {
     });
     await store.dispatch("stimulation/handle_selected_wells", [false, true, false, true]);
     const options = wrapper.findAll("li");
-    await options.at(1).trigger("click");
+    await options.at(0).trigger("click");
     await wrapper.vm.handle_click(0);
     expect(store.state.stimulation.protocol_assignments[1]).toBeTruthy();
     await wrapper.vm.handle_click(1);
     expect(store.state.stimulation.protocol_assignments[1]).toBeFalsy();
+  });
+
+  test("When a user imports a new protocol, Then the the available protocol list in dropdown will get updated", async () => {
+    const updateSpy = jest.spyOn(StimulationStudioCreateAndEdit.methods, "update_protocols");
+    mount(StimulationStudioCreateAndEdit, {
+      store,
+      localVue,
+    });
+    const test_protocol = { label: "test", color: "#123456", letter: "B" };
+    await store.commit("stimulation/set_imported_protocol", test_protocol);
+    expect(updateSpy).toHaveBeenCalledWith();
+  });
+
+  test("When exiting instance, Then instance is effectively destroyed", async () => {
+    const destroyed_spy = jest.spyOn(StimulationStudioCreateAndEdit, "beforeDestroy");
+    const wrapper = mount(StimulationStudioCreateAndEdit, {
+      store,
+      localVue,
+    });
+    wrapper.destroy();
+    expect(destroyed_spy).toHaveBeenCalledWith();
+  });
+
+  test("When clicks on export protocol button, Then action will be dispatched to store", async () => {
+    const export_spy = jest.spyOn(StimulationStudioCreateAndEdit.methods, "handle_export");
+    window.webkitURL.createObjectURL = function () {};
+
+    const wrapper = mount(StimulationStudioCreateAndEdit, {
+      store,
+      localVue,
+    });
+    await wrapper.findAll(".span__stimulationstudio-btn-label").at(3).trigger("click");
+
+    expect(export_spy).toHaveBeenCalledTimes(1);
+  });
+
+  test("When clicks on import protocol button, Then action will be dispatched to store", async () => {
+    const import_spy = jest
+      .spyOn(FileReader.prototype, "readAsText")
+      .mockImplementation(() => "test successful");
+    const wrapper = mount(StimulationStudioCreateAndEdit, {
+      store,
+      localVue,
+    });
+
+    await wrapper.findAll("input").at(0).trigger("change");
+
+    expect(import_spy).toHaveBeenCalledTimes(1);
   });
 
   test("When a user clicks on Clear All to reset new protocol, Then the dropdown should reset to default option", async () => {
