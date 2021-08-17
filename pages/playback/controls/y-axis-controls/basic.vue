a084b6c76c31222991eb15d16e4262026b872cbb
Luci Pak <pak.luci.13@gmail.com>
2021-08-17 11:07:32 -0700

    added page with y axis controls and graph

--- pages/playback/controls/y-axis-controls/basic.vue
+++ pages/playback/controls/y-axis-controls/basic.vue
@@ -6,8 +6,6 @@
 
 <script>
 import { YAxisControls } from "@/dist/mantarray.common";
-// import YAxisControls from "@/components/playback/controls/YAxisControls.vue";
-// import ContinuousWaveform from "@/components/playback/waveform/ContinuousWaveform.vue";
 
 export default {
   components: {
