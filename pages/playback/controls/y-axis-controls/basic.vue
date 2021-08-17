08a9a3db9824f540e119bb3e4a47f7ecc3005953
Luci Pak <pak.luci.13@gmail.com>
2021-08-17 15:10:46 -0700

    added button to YAxisControls to update window size

--- pages/playback/controls/y-axis-controls/basic.vue
+++ pages/playback/controls/y-axis-controls/basic.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <YAxisControls />
+    <button id="min_button" class="update-button" @click="update_y_zoom_min">Update min</button>
+    <button id="max_button" class="update-button" :style="'top: 200px;'" @click="update_y_zoom_max">
+      Update max
+    </button>
   </div>
 </template>
 
@@ -11,5 +15,33 @@ export default {
   components: {
     YAxisControls,
   },
+  methods: {
+    update_y_zoom_min() {
+      const new_range = { min: 2, max: 4, midpoint: 3 };
+      this.$store.commit("waveform/set_y_axis", new_range);
+    },
+    update_y_zoom_max() {
+      const new_range = { min: -201, max: 100001, midpoint: 49900 };
+      this.$store.commit("waveform/set_y_axis", new_range);
+    },
+  },
 };
 </script>
+<style scoped>
+.update-button {
+  position: absolute;
+  top: 100px;
+  left: 2000px;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  background-color: #4ca0af;
+  z-index: 999;
+}
+</style>
--- tests/e2e/playback/controls/y-axis-controls.e2e.js
+++ tests/e2e/playback/controls/y-axis-controls.e2e.js
@@ -6,6 +6,8 @@ import { testcafe_page_visual_regression } from "@curi-bio/frontend-test-utils";
 
 const zoom_in_button = Selector(".span__playback-y-axis-controls-zoom-in-button");
 const zoom_out_button = Selector(".span__playback-y-axis-controls-zoom-out-button");
+const set_max_window = Selector("#max_button");
+const set_min_window = Selector("#min_button");
 
 // the fixture declares what we are testing
 fixture`playback/controls/y-axis-controls/basic`
@@ -31,7 +33,7 @@ test("y-axis controls have both buttons enabled and  zoom_out on hover and verif
 });
 
 test("y-axis controls have both buttons enabled initially and we hover on the zoom_out_button", async (t) => {
-  await t.click(zoom_out_button);
+  await t.click(set_max_window);
   const screenshot_path = path.join("playback", "controls", "y-axis-controls", "basic-init-out-max-hover");
   await t.hover(zoom_out_button);
 
@@ -40,7 +42,7 @@ test("y-axis controls have both buttons enabled initially and we hover on the zo
 });
 
 test("y-axis controls have both buttons enabled and reached max zoom_out and verify the tooltips on the zoom_out_button", async (t) => {
-  await t.click(zoom_out_button);
+  await t.click(set_max_window);
   const screenshot_path = path.join("playback", "controls", "y-axis-controls", "basic-init-out-max-tooltips");
   await t.hover(zoom_out_button);
   await t.wait(2000);
@@ -62,7 +64,7 @@ test("y-axis controls have both buttons enabled initially and verify the tooltip
 });
 
 test("y-axis controls have both buttons enabled enabled and reached max and verify the tooltips zoom_in_button", async (t) => {
-  await t.click(zoom_in_button);
+  await t.click(set_min_window);
   const screenshot_path = path.join("playback", "controls", "y-axis-controls", "basic-init-in-max-tooltips");
   await t.hover(zoom_in_button);
   await t.wait(2000);
@@ -71,7 +73,7 @@ test("y-axis controls have both buttons enabled enabled and reached max and veri
 });
 
 test("clicking on zoom in to bring to max zoom level causes zoom in button to render as disabled", async (t) => {
-  await t.click(zoom_in_button);
+  await t.click(set_min_window);
   const screenshot_path = path.join("playback", "controls", "y-axis-controls", "basic-max-zoom-in");
   await testcafe_page_visual_regression(t, screenshot_path);
   // restore to initial state
@@ -79,7 +81,7 @@ test("clicking on zoom in to bring to max zoom level causes zoom in button to re
 });
 
 test("clicking on zoom out to bring to min zoom level causes zoom out button to render as disabled", async (t) => {
-  await t.click(zoom_out_button);
+  await t.click(set_max_window);
   const screenshot_path = path.join("playback", "controls", "y-axis-controls", "basic-max-zoom-out");
   await testcafe_page_visual_regression(t, screenshot_path);
   await t.click(zoom_in_button);
