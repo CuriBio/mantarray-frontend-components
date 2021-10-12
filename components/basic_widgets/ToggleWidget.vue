085d1ff4e353dd5c43ce882c012b8117b3b4010c
Luci Pak <pak.luci.13@gmail.com>
2021-10-12 15:42:13 -0700

    added toggle components

---
+++ components/basic_widgets/ToggleWidget.vue
@@ -0,0 +1,132 @@
+<template>
+  <label class="switch">
+    <input type="checkbox" :checked="checked" @click="toggle_checkbox" />
+    <div class="slider round" />
+  </label>
+</template>
+<script>
+export default {
+  name: "ToggleWidget",
+  props: {
+    checked_state: { type: Boolean, default: false },
+    label: { type: String, required: true }
+  },
+  data() {
+    return {
+      checked: false
+    };
+  },
+  watch: {
+    checked_state: function() {
+      this.checked = this.checked_state;
+    }
+  },
+  created() {
+    this.checked = this.checked_state;
+  },
+  methods: {
+    toggle_checkbox() {
+      this.checked = !this.checked;
+      this.$emit("handle_toggle_state", this.checked, this.label);
+    }
+  }
+};
+</script>
+<style type="text/css">
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 50px;
+  height: 24px;
+}
+
+.switch input {
+  display: none;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #4c4c4c;
+  -webkit-transition: 0.6s;
+  transition: 0.6s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  left: 4px;
+  bottom: 4px;
+  background-color: #6e6f72;
+  -webkit-transition: 0.6s;
+  transition: 0.6s;
+}
+
+input:checked + .slider {
+  background-color: #a7a8a9;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #a7a8a9;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+  background-color: #ececed;
+}
+
+.round {
+  border-radius: 24px;
+}
+
+.round:before {
+  border-radius: 50%;
+}
+
+.slider_checked {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #a7a8a9;
+  -webkit-transition: 0.6s;
+  transition: 0.6s;
+}
+
+.slider_checked:before {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  left: 30px;
+  bottom: 4px;
+  background-color: #ececed;
+  -webkit-transition: 0.6s;
+  transition: 0.6s;
+}
+
+input:checked + .slider_checked {
+  background-color: #4c4c4c;
+}
+
+input:focus + .slider_checked {
+  box-shadow: 0 0 1px #4c4c4c;
+}
+
+input:checked + .slider_checked:before {
+  -webkit-transform: translateX(-26px);
+  -ms-transform: translateX(-26px);
+  transform: translateX(-26px);
+  background-color: #6e6f72;
+}
+</style>
