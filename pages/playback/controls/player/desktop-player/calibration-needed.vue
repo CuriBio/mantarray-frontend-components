158654f7b0c34af6085b5d1d7ad5497240284732
Luci Pak <pak.luci.13@gmail.com>
2021-10-13 08:46:32 -0700

    added update customer idx button for e2e testing

--- pages/playback/controls/player/desktop-player/calibration-needed.vue
+++ pages/playback/controls/player/desktop-player/calibration-needed.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
-    <DesktopPlayerControls></DesktopPlayerControls>
+    <DesktopPlayerControls />
+    <button class="update-idx-button" @click="update_customer_idx">Update customer idx</button>
   </div>
 </template>
 
 <script>
-import { DesktopPlayerControls } from "@/dist/mantarray.common";
-// import DesktopPlayerControls from "@/components/playback/controls/player/DesktopPlayerControls.vue";
+// import { DesktopPlayerControls } from "@/dist/mantarray.common";
+import DesktopPlayerControls from "@/components/playback/controls/player/DesktopPlayerControls.vue";
 import playback_module from "@/store/modules/playback";
 export default {
   components: {
-    DesktopPlayerControls,
+    DesktopPlayerControls
   },
-  created: function () {
+  created: function() {
     this.$store.commit(
       "playback/set_playback_state",
       playback_module.ENUMS.PLAYBACK_STATES.CALIBRATION_NEEDED
@@ -20,12 +21,35 @@ export default {
 
     window.addEventListener(
       "keypress",
-      function (e) {
+      function(e) {
         this.$store.commit("flask/stop_status_pinging");
       }.bind(this)
     );
     // set a valid barcode so that live view can be initiated during the tests
     this.$store.commit("playback/set_barcode_number", "MA200440004");
   },
+  methods: {
+    update_customer_idx: function() {
+      this.$store.commit("settings/set_customer_index", 0);
+    }
+  }
 };
 </script>
+<style scoped>
+.update-idx-button {
+  position: absolute;
+  top: 100px;
+  left: 1840px;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  background-color: #4ca0af;
+  z-index: 999;
+}
+</style>
