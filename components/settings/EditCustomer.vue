2ac0625811c447df01981f6f44592d4e7964cf0d
Luci Pak <pak.luci.13@gmail.com>
2021-10-14 16:29:03 -0700

    grabbing new screenshots

--- components/settings/EditCustomer.vue
+++ components/settings/EditCustomer.vue
@@ -19,7 +19,7 @@
 
     <div id="passkey" style="top: 145px; left: 50px; position: absolute; z-index: 23">
       <InputWidget
-        :title_label="'pass Key'"
+        :title_label="'Enter Pass-Key'"
         :placeholder="'ba86b8f0-6fdf-4944-87a0-8a491a19490e'"
         :invalid_text="error_text_pass"
         :initial_value="pass_key"
