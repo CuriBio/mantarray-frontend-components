72313721e80954b18c7ad2393e0e35313d8f4c73
luciipak <luciypak@gmail.com>
2022-09-29 15:08:06 +1000

    updated unit tests

--- tests/unit/store/flask.spec.js
+++ tests/unit/store/flask.spec.js
@@ -7,6 +7,7 @@ import { STATUS } from "@/store/modules/flask/enums";
 import playback_module from "@/store/modules/playback";
 import { ping_system_status } from "../../../store/modules/flask/actions";
 import { system_status_regexp } from "@/store/modules/flask/url_regex";
+import { STIM_STATUS } from "../../../store/modules/stimulation/enums";
 
 describe("store/flask", () => {
   const localVue = createLocalVue();
@@ -279,7 +280,8 @@ describe("store/flask", () => {
       beforeEach(async () => {
         context = await store.dispatch("flask/get_flask_action_context");
       });
-      test("Given that /system_status is mocked to include an is_stimulating value, When the ping_system_status is active, then stimulation/set_stim_play_state is called with that value", async () => {
+
+      test("Given that /system_status is mocked to include an is_stimulating value, When the ping_system_status is active, Then stimulation/set_stim_play_state is called with that value", async () => {
         mocked_axios.onGet(system_status_regexp).reply(200, {
           ui_status_code: STATUS.MESSAGE.CALIBRATION_NEEDED,
           in_simulation_mode: false,
@@ -296,6 +298,23 @@ describe("store/flask", () => {
         });
         await bound_ping_system_status();
         expect(store.state.stimulation.stim_play_state).toBe(false);
+      });
+      test("Given a protocol was run with setting 'stimulate until complete' and stimulation is STIM_ACTIVE, When ping_system_status returns an is_stimulating value of false, Then the stim state state should be updated to READY", async () => {
+        // to have ACTIVE status, needs to have protocols assigned
+        store.state.stimulation.protocol_assignments = { A: {}, B: {} };
+        store.state.stimulation.stim_status = STIM_STATUS.STIM_ACTIVE;
+
+        mocked_axios.onGet(system_status_regexp).reply(200, {
+          ui_status_code: STATUS.MESSAGE.LIVE_VIEW_ACTIVE,
+          in_simulation_mode: false,
+          is_stimulating: false,
+        });
+
+        const bound_ping_system_status = ping_system_status.bind(context);
+        await bound_ping_system_status();
+
+        expect(store.state.stimulation.stim_play_state).toBe(false);
+        expect(store.state.stimulation.stim_status).toBe(STIM_STATUS.READY);
       });
     });
   });
