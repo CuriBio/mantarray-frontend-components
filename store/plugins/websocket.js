e84e4417dd62df39e3d57766a7a146386fc1a0b5
luciipak <luciypak@gmail.com>
2022-09-29 14:37:10 +1000

    removed console.log

--- store/plugins/websocket.js
+++ store/plugins/websocket.js
@@ -54,7 +54,6 @@ export default function create_web_socket_plugin(socket) {
       // TODO change this message type to stimulation_data
       // Tanner (12/20/21): may want to put the same checks here as are in the waveform_data handler once stim waveforms are sent instead of subprotocol indices
       store.dispatch("data/append_stim_waveforms", JSON.parse(stim_json));
-      console.log("JSON.parse(stim_json)", JSON.parse(stim_json));
       /* istanbul ignore else */
       if (cb) cb("action done"); // this callback is only used for testing. The backend will not send a callback
     });
