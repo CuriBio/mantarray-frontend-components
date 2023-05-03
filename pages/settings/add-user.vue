b1b3e5fcb9a3f11b2323cf86d40b321095e4975b
Luci Pak <luciypak@gmail.com>
2023-05-03 13:14:36 +1000

    Remove unnecessary test files

---
+++ pages/settings/add-user.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <AddUser> </AddUser>
+  </div>
+</template>
+
+<script>
+// import { AddUser } from "@/dist/mantarray.common";
+import AddUser from "@/components/settings/AddUser.vue";
+export default {
+  components: {
+    AddUser,
+  },
+};
+</script>
---
+++ pages/settings/edit-user.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <EditUser :dialogdata="customer"> </EditUser>
+  </div>
+</template>
+
+<script>
+import { EditUser } from "@/dist/mantarray.common";
+// import EditUser from "@/components/settings/EditUser.vue";
+export default {
+  components: {
+    EditUser,
+  },
+  data() {
+    return {
+      customer: {
+        customer_id: "0",
+        uuid: "5FY8KwTsQaUJ2KzHJGetfE",
+        user_password: "06ad547f-fe02-477b-9473-f7977e4d5e17",
+        user_name: "Experiment anemia -1",
+      },
+    };
+  },
+};
+</script>
